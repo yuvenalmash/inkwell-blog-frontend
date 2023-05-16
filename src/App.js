c3059d4d43104bae457b4c8683d6aa5ef978f7e9
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-05-17 00:41:42 +0300

    Install and setup tailwinds

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './components/home/Homepage';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="mx-auto px-4 sm:px-6 lg:px-8 bg-gray-100 min-h-screen">
       <Router>
         <Routes>
           <Route path="/" element={<Homepage />} />
