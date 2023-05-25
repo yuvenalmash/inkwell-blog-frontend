66f650dedb1f2b2ad4234ddfef7fa236c1045925
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-05-25 20:07:35 +0300

    configure tailwinds

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  mode: 'jit',
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
+    screen: {
+      'xs': '475px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
   },
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html'
+  ],
   variants: {
     extend: {},
   },
