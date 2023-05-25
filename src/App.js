c317c3fe52b68c1eb893f3f9fef4d92a838b3432
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-05-25 20:08:22 +0300

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { loginUser } from './redux/slices/authenticationSlice';
 import Homepage from './components/home/Homepage';
+import Signup from './components/authentication/Register';
+import Login from './components/authentication/Login';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loginUser());
+  }, [dispatch]);
+
   return (
-    <div className="mx-auto px-4 sm:px-6 lg:px-8 bg-gray-100 min-h-screen">
+    <div className="flex flex-col max-w-md mx-auto min-h-screen bg-slate-100 shadow-md overflow-hidden md:max-w-2xl">
       <Router>
         <Routes>
           <Route path="/" element={<Homepage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
