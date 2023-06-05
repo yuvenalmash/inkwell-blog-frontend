da9b69d523815429348220f21f8dbf6dda996bd4
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-06-06 00:55:15 +0300

    Update styling

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ import Signup from './components/authentication/Signup';
 import Login from './components/authentication/Login';
 
 const App = () => (
-  <div className="flex flex-col max-w-md mx-auto min-h-screen bg-slate-100 shadow-md overflow-hidden md:max-w-2xl">
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/signup" element={<SignupWrapper />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+
+  <div className="bg-slate-100 min-h-screen">
+    <div className="flex flex-col max-w-lg mx-auto min-h-screen bg-slate-200 shadow-md overflow-hidden md:max-w-4xl">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/signup" element={<SignupWrapper />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
   </div>
 );
 
--- src/components/home/Homepage.jsx
+++ src/components/home/Homepage.jsx
@@ -2,41 +2,45 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { selectUser, logoutUser } from '../../redux/slices/authenticationSlice';
+import Navbar from '../navbar/Navbar';
 
 const Homepage = () => {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
 
   const handleLogout = () => {
-    dispatch(logoutUser(user.id));
+    dispatch(logoutUser());
   };
 
   return (
-    <div className="flex flex-col h-screen items-center justify-center gap-5">
-      <h1 className="text-3xl font-bold text-gray-900">Home</h1>
-      {user ? (
-        <div className="flex flex-col items-center justify-center">
-          <p className="text-gray-900">
-            Welcome,
-            {' '}
-            {user.username}
-          </p>
-          <button type="button" onClick={handleLogout} className="text-blue-500">Log Out</button>
-        </div>
-      ) : (
-        <div className="flex flex-col items-center justify-center">
-          <p className="text-gray-900">
-            You are not logged in.
-          </p>
-          <Link to="/login" className="text-blue-500">Log In</Link>
-          <p className="text-gray-900">
-            Don&apos;t have an account?
-            {' '}
-            <Link to="/signup" className="text-blue-500">Sign Up</Link>
-          </p>
-        </div>
-      )}
-    </div>
+    <>
+      <Navbar />
+      <div className="flex flex-col h-screen items-center justify-center gap-5">
+        <h1 className="text-3xl font-bold text-gray-900">Home</h1>
+        {user ? (
+          <div className="flex flex-col items-center justify-center">
+            <p className="text-gray-900">
+              Welcome,
+              {' '}
+              {user.username}
+            </p>
+            <button type="button" onClick={handleLogout} className="text-blue-500">Log Out</button>
+          </div>
+        ) : (
+          <div className="flex flex-col items-center justify-center">
+            <p className="text-gray-900">
+              You are not logged in.
+            </p>
+            <Link to="/login" className="text-blue-500">Log In</Link>
+            <p className="text-gray-900">
+              Don&apos;t have an account?
+              {' '}
+              <Link to="/signup" className="text-blue-500">Sign Up</Link>
+            </p>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
