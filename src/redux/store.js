0577c2e4b69e9d1993629db6b65fd35d6b34e10e
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-06-11 23:51:39 +0300

    Add postsReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authenticationReducer from './slices/authenticationSlice';
+import postsReducer from './slices/postsSlice';
 
 const store = configureStore({
   reducer: {
     authentication: authenticationReducer,
+    posts: postsReducer,
   },
 });
 
