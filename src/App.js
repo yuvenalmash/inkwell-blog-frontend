8e5d8ded42a9d0198255aad444f6de1cb0a4e2a5
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-06-11 23:10:26 +0300

    remove unused div

--- src/App.js
+++ src/App.js
@@ -7,17 +7,14 @@ import Signup from './components/authentication/Signup';
 import Login from './components/authentication/Login';
 
 const App = () => (
-
-  <div className="bg-slate-100 min-h-screen">
-    <div className="flex flex-col max-w-lg mx-auto min-h-screen bg-slate-200 shadow-md overflow-hidden md:max-w-4xl">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/signup" element={<SignupWrapper />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Router>
-    </div>
+  <div className="flex flex-col mx-auto min-h-screen bg-slate-200 shadow-md overflow-hidden">
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/signup" element={<SignupWrapper />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   </div>
 );
 
